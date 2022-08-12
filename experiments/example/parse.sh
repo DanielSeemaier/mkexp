4c884222536cf5d63f3aacb6f502c98428ce5d52
Daniel Seemaier <daniel.seemaier@kit.edu>
2022-08-12 17:04:58 +0200

    Add ParHIP

--- experiments/example/parse.sh
+++ experiments/example/parse.sh
@@ -1,1 +1,2 @@
 #!/usr/bin/env bash
+ParseLogs "ParHIP" "/home/seemaier/projects/distributed-graph-partitioner/experiments/example/logs/ParHIP-Eco" > "/home/seemaier/projects/distributed-graph-partitioner/experiments/example/results/ParHIP-Eco.csv"
