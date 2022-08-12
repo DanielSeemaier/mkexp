eb68a7a71cb68f7ea0bc60371afe47c33ce86d41
Daniel Seemaier <daniel.seemaier@kit.edu>
2022-08-12 16:52:23 +0200

    .

--- experiments/example/parse.sh
+++ experiments/example/parse.sh
@@ -1,3 +1,1 @@
 #!/usr/bin/env bash
-ParseLogs "dKaMinPar" "/home/seemaier/projects/distributed-graph-partitioner/experiments/example/logs/dKaMinPar" > "/home/seemaier/projects/distributed-graph-partitioner/experiments/example/results/dKaMinPar.csv
-ParseLogs "dKaMinPar" "/home/seemaier/projects/distributed-graph-partitioner/experiments/example/logs/dKaMinPar-FM" > "/home/seemaier/projects/distributed-graph-partitioner/experiments/example/results/dKaMinPar-FM.csv
