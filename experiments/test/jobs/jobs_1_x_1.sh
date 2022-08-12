144da02c60dc13aa4670402ea7d68f61b1e0d7a1
Daniel Seemaier <daniel.seemaier@kit.edu>
2022-08-12 11:42:42 +0200

    .

---
+++ experiments/test/jobs/jobs_1_x_1.sh
@@ -0,0 +1,74 @@
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/144.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/3elt.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/4elt.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/598a.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/add20.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/add32.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/amazon.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/auto.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/bcsstk29.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/bcsstk30.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/bcsstk31.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/bcsstk32.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/bcsstk33.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/brack2.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/crack.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/cs4.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/cti.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/data.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/europe.osm.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/fe_4elt2.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/fe_body.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/fe_ocean.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/fe_pwt.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/fe_rotor.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/fe_sphere.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/fe_tooth.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/finan512.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/germany.osm.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/m14b.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/memplus.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/t60k.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/uk.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/vibrobox.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/wave.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/whitaker3.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/wing.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/wing_nodal.graph -k 4 -e 0.03 --seed=0 
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/144.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/3elt.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/4elt.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/598a.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/add20.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/add32.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/amazon.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/auto.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/bcsstk29.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/bcsstk30.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/bcsstk31.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/bcsstk32.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/bcsstk33.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/brack2.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/crack.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/cs4.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/cti.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/data.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/europe.osm.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/fe_4elt2.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/fe_body.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/fe_ocean.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/fe_pwt.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/fe_rotor.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/fe_sphere.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/fe_tooth.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/finan512.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/germany.osm.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/m14b.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/memplus.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/t60k.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/uk.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/vibrobox.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/wave.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/whitaker3.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/wing.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
+/home/seemaier/projects/distributed-graph-partitioner/usr//bin/dKaMinPar -G /G/wing_nodal.graph -k 4 -e 0.03 --seed=0 --r-algorithm=prob-lp+fm
