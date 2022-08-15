e1ad7965008d3c6ba94aa064035fb72bbbd1e22e
Daniel Seemaier <daniel@seemaier.de>
2022-08-15 08:13:14 +0200

    Move experiments to their own repository

--- experiments/example/parse.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-ParseLogs "ParHIP" "/home/seemaier/projects/distributed-graph-partitioner/experiments/example/logs/ParHIP-Eco" > "/home/seemaier/projects/distributed-graph-partitioner/experiments/example/results/ParHIP-Eco.csv"
