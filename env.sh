485366b08cc773a5e3f1e704e872cf7c394ca95e
Daniel Seemaier <daniel@seemaier.de>
2022-08-15 08:15:12 +0200

    Make source env.sh work from any PWD

--- env.sh
+++ env.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+source=${BASH_SOURCE[0]}
+while [ -L "$source" ]; do
+  script_pwd=$(cd -P "$(dirname "$source")" >/dev/null 2>&1 && pwd)
+  source=$(readlink "$source")
+  [[ $source != /* ]] && source=$script_pwd/$source
+done
+script_pwd=$(cd -P "$(dirname "$source")" >/dev/null 2>&1 && pwd)
+
 # Scripts contained in this project
-export PATH="$PWD/bin:$PATH"
+export PATH="$script_pwd/bin:$PATH"
 
 # Install directories for graph partitioners
-export DGP_PREFIX="$PWD/usr/"
+export DGP_PREFIX="$script_pwd/usr/"
 
 export PATH="$DGP_PREFIX/bin:$PATH"
 export LD_LIBRARY_PATH="$DGP_PREFIX/lib:$LD_LIBRARY_PATH"
@@ -12,4 +20,4 @@ export C_INCLUDE_PATH="$DGP_PREFIX/include:$C_INCLUDE_PATH"
 export CPLUS_INCLUDE_PATH="$DGP_PREFIX/include:$CPLUS_INCLUDE_PATH"
 
 # Load private variables
-[[ -f private ]] && . private
+[[ -f "$script_pwd/private" ]] && . "$script_pwd/private"
