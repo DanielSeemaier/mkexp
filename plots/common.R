5f240c618878787130a108b5e75fef2c397d4b2e
Daniel Seemaier <daniel.seemaier@kit.edu>
2023-05-08 18:59:19 +0200

    .

--- plots/common.R
+++ plots/common.R
@@ -129,20 +129,22 @@ DEFAULT_ASPECT_RATIO <- 2 / (1 + sqrt(5))
 
 create_theme_facet <- function(aspect_ratio = DEFAULT_ASPECT_RATIO) {
   theme(
-    aspect.ratio = aspect_ratio,
+    #aspect.ratio = aspect_ratio,
     legend.background = element_blank(),
     legend.title = element_blank(),
     legend.box.spacing = unit(0.1, "cm"),
     legend.title.align = 0.5,
-    legend.text = element_text(size = 8, color = "black"),
-    plot.title = element_text(size = 10, hjust = 0.5, color = "black"),
+    legend.text = element_text(size = 12, color = "black"),
+    plot.title = element_text(size = 14, hjust = 0.5, color = "black"),
     panel.grid.major = element_line(linetype = "11", linewidth = 0.5, color = "grey"),
     panel.grid.minor = element_blank(),
+    strip.background = element_blank(),
+    strip.text = element_text(size = 14),
     axis.line = element_line(linewidth = 0.2, color = "black"),
-    axis.title.y = element_text(size = 8, vjust = 1.5, color = "black"),
-    axis.title.x = element_text(size = 8, vjust = 1.5, color = "black"),
-    axis.text.x = element_text(angle = 0, hjust = 0.5, size = 8, color = "black"),
-    axis.text.y = element_text(size = 8, color = "black")
+    axis.title.y = element_text(size = 12, vjust = 1.5, color = "black"),
+    axis.title.x = element_text(size = 12, vjust = 1.5, color = "black"),
+    axis.text.x = element_text(size = 12, angle = 0, hjust = 0.5, color = "black"),
+    axis.text.y = element_text(size = 12, color = "black")
   )
 }
 
