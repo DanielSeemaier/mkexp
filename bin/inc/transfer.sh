2d7d656cb8c5e8e9e0d8f4fc0b8d182d13bf22cb
Daniel Seemaier <daniel@seemaier.de>
2023-11-16 18:19:40 +0100

    Add info text after --self-upload

--- bin/inc/transfer.sh
+++ bin/inc/transfer.sh
@@ -3,9 +3,15 @@ if [[ $mode == "upload" ]]; then
     name="${dir##*/}"
     UploadDirectory "$dir"/ "$name"
 fi
+
 if [[ $mode == "upload-self" ]]; then
     UploadDirectory "$ROOT"/ mkexp
+
+    echo "Uploaded mkexp framework to the remote host"
+    echo "  You can now add the bin directory to your PATH environment variable, e.g., by running on the remote host:"
+    echo "  echo 'export PATH=\$PATH:~/mkexp/bin' >> ~/.bashrc"
 fi
+
 if [[ $mode == "download" ]]; then
     dir="$(pwd)"
     name="${dir##*/}"
