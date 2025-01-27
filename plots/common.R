52de9e93ebb4baf58589e0d63c3057dd39f21961
Daniel Seemaier <daniel@seemaier.de>
2025-01-27 16:36:32 +0100

    refactor(plots): make extra columns more flexible, rename Balance to Imbalance, add detailed timing plots

--- plots/common.R
+++ plots/common.R
@@ -3,237 +3,239 @@
 
 DEPS <- c("ggplot2", "plyr", "dplyr", "RColorBrewer", "gridExtra", "egg")
 for (dep in DEPS) {
-  if (!require(dep, character.only = TRUE, warn.conflicts = FALSE)) {
-    install.packages(dep)
-  }
+        if (!require(dep, character.only = TRUE, warn.conflicts = FALSE)) {
+                install.packages(dep)
+        }
 }
 
 empty_min <- function(x) {
-  if (length(x[!is.na(x)]) > 0) {
-    return(min(x, na.rm = TRUE))
-  } else {
-    return(Inf)
-  }
+        if (length(x[!is.na(x)]) > 0) {
+                return(min(x, na.rm = TRUE))
+        } else {
+                return(Inf)
+        }
 }
 
-weakscaling_aggregator <- function(df) {
-  data.frame(
-    MinCut = empty_min(df$Cut),
-    AvgCut = mean(df$Cut, na.rm = TRUE),
-    MinTime = empty_min(df$Time),
-    AvgTime = mean(df$Time, na.rm = TRUE),
-    MinBalance = empty_min(df$Balance),
-    M = max(df$M, na.rm = TRUE),
-    N = max(df$N, na.rm = TRUE),
-    Timeout = any(as.logical(df$Timeout)) & all(as.logical(df$Timeout) | as.logical(df$Failed)),
-    Failed = all(as.logical(df$Failed))
-  )
+OPTIONAL_COLUMNS <- c(
+        "MaxRSS",
+        "TimeIO",
+        "TimeRefinement",
+        "TimeRefinementLabelPropagation",
+        "TimeRefinementToplevel",
+        "TimeRefinementToplevelLabelPropagation",
+        "TimeRefinementCoarse",
+        "TimeRefinementCoarseLabelPropagation",
+        "TimeCoarsening",
+        "TimeCoarseningLabelPropagation",
+        "TimeCoarseningToplevel",
+        "TimeCoarseningToplevelLabelPropagation",
+        "TimeCoarseningCoarse",
+        "TimeCoarseningCoarseLabelPropagation"
+)
+
+weakscaling_aggregator <- function(group) {
+        df <- data.frame(
+                MinCut = empty_min(group$Cut),
+                AvgCut = mean(group$Cut, na.rm = TRUE),
+                MinTime = empty_min(group$Time),
+                AvgTime = mean(group$Time, na.rm = TRUE),
+                MinImbalance = empty_min(group$Imbalance),
+                M = max(group$M, na.rm = TRUE),
+                N = max(group$N, na.rm = TRUE),
+                Timeout = any(as.logical(group$Timeout)) & all(as.logical(group$Timeout) | as.logical(group$Failed)),
+                Failed = all(as.logical(group$Failed))
+        )
+        for (col in OPTIONAL_COLUMNS) {
+                if (col %in% colnames(group)) {
+                        df <- df %>% dplyr::mutate(!!paste0("Avg", col) := mean(group[[col]], na.rm = TRUE))
+                }
+        }
+        return(df)
 }
 
-aggregate_data <- function(df, timelimit, aggregator, ignore_first_seed = FALSE) {
-  if (!("Timeout" %in% colnames(df))) {
-    df$Timeout <- FALSE
-  }
-  if (!("Failed" %in% colnames(df))) {
-    df$Failed <- FALSE
-  }
-  if (!("Epsilon" %in% colnames(df))) {
-    df$Epsilon <- 0.03
-  }
-  if (!("Seed" %in% colnames(df))) {
-    df$Seed <- 0
-  }
-  if (!("GCSize" %in% colnames(df))) {
-    df$GCSize <- 0
-  }
-  if (!("MaxRSS" %in% colnames(df))) {
-    df$MaxRSS <- 0
-  }
+aggregate_data <- function(df, timelimit, aggregator) {
+        df <- df %>%
+                dplyr::mutate(Cut = ifelse(Timeout | Failed, NA, Cut)) %>%
+                dplyr::mutate(Imbalance = ifelse(Timeout | Failed, NA, Imbalance)) %>%
+                dplyr::mutate(Time = ifelse(Timeout, timelimit, Time)) %>%
+                dplyr::mutate(Time = ifelse(Failed & !Timeout, NA, Time)) %>%
+                dplyr::mutate(Cut = ifelse(Imbalance > Epsilon + .Machine$double.eps, NA, Cut))
 
-  if (ignore_first_seed) {
-    df <- df %>% dplyr::filter(Seed > 0)
-  }
+        vars <- colnames(df)
+        vars <- vars[!vars %in% c("Seed", "Cut", "Time", "Imbalance", "Timeout", "Failed", OPTIONAL_COLUMNS)]
+        df <- ddply(df, vars, aggregator)
 
-  df <- df %>%
-    dplyr::mutate(Cut = ifelse(Timeout | Failed, NA, Cut)) %>%
-    dplyr::mutate(Balance = ifelse(Timeout | Failed, NA, Balance)) %>%
-    dplyr::mutate(Time = ifelse(Timeout, timelimit, Time)) %>%
-    dplyr::mutate(Time = ifelse(Failed & !Timeout, NA, Time)) %>%
-    dplyr::mutate(Cut = ifelse(Balance > Epsilon + .Machine$double.eps, NA, Cut))
+        df <- df %>%
+                dplyr::mutate(AvgCut = ifelse(is.na(AvgCut), Inf, AvgCut)) %>%
+                dplyr::mutate(MinCut = ifelse(is.na(MinCut), Inf, MinCut)) %>%
+                dplyr::mutate(AvgTime = ifelse(is.na(AvgTime), Inf, AvgTime)) %>%
+                dplyr::mutate(MinTime = ifelse(is.na(MinTime), Inf, MinTime))
 
-  vars <- colnames(df)
-  vars <- vars[!vars %in% c("Cut", "Balance", "Time", "Failed", "Timeout", "Seed", "MaxRSS", "TimeTotal", "TimeIO")]
-  df <- ddply(df, vars, aggregator)
+        df <- df %>%
+                dplyr::mutate(Infeasible = !Failed &
+                        !Timeout &
+                        MinImbalance > Epsilon + .Machine$double.eps) %>%
+                dplyr::mutate(Feasible = !Failed & !Timeout & !Infeasible) %>%
+                dplyr::mutate(Invalid = Failed | Timeout | Infeasible)
 
-  df <- df %>%
-    dplyr::mutate(AvgCut = ifelse(is.na(AvgCut), Inf, AvgCut)) %>%
-    dplyr::mutate(MinCut = ifelse(is.na(MinCut), Inf, MinCut)) %>%
-    dplyr::mutate(AvgTime = ifelse(is.na(AvgTime), Inf, AvgTime)) %>%
-    dplyr::mutate(MinTime = ifelse(is.na(MinTime), Inf, MinTime))
-
-  df <- df %>%
-    dplyr::mutate(Infeasible = !Failed &
-      !Timeout &
-      MinBalance > Epsilon + .Machine$double.eps) %>%
-    dplyr::mutate(Feasible = !Failed & !Timeout & !Infeasible) %>%
-    dplyr::mutate(Invalid = Failed | Timeout | Infeasible)
-
-  return(df)
+        return(df)
 }
 
 load_data <- function(name, file, ignore_balance = FALSE, seed = 0) {
-  full_filename <- paste0(getwd(), "/", file)
-  df <- read.csv(full_filename)
-  df <- df %>% dplyr::filter(Seed >= seed)
-  cat(paste0("Read algorithm ", name, " from ", file, ": ", nrow(df), " runs\n"))
+        full_filename <- paste0(getwd(), "/", file)
+        df <- read.csv(full_filename)
+        df <- df %>% dplyr::filter(Seed >= seed)
+        cat(paste0("Read algorithm ", name, " from ", file, ": ", nrow(df), " runs\n"))
 
-  # Normalize columns
-  if (!("NumNodes" %in% colnames(df))) {
-    df$NumNodes <- 1
-  }
-  if (!("NumMPIsPerNode" %in% colnames(df))) {
-    df$NumMPIsPerNode <- 1
-  }
-  if (!("NumThreadsPerMPI" %in% colnames(df))) {
-    if ("Threads" %in% colnames(df)) {
-      df$NumThreadsPerMPI <- df$Threads
-    } else if ("NumThreads" %in% colnames(df)) {
-      df$NumThreadsPerMPI <- df$NumThreads
-    } else {
-      df$NumThreadsPerMPI <- 1
-    }
-  }
-  if (!("Timeout" %in% colnames(df))) {
-    df$Timeout <- FALSE
-  }
-  if (!("Failed" %in% colnames(df))) {
-    df$Failed <- FALSE
-  }
-  if (!("Epsilon" %in% colnames(df))) {
-    print("Warning: no Epsilon column; default to 3%")
-    df$Epsilon <- 0.03
-  }
-  if (ignore_balance) {
-    df$Epsilon <- 10000.0
-  }
-  if (!("Balance" %in% colnames(df)) & "Imbalance" %in% colnames(df)) {
-    df$Balance <- df$Imbalance
-  } else if (!("Balance" %in% colnames(df))) {
-    print("Warning: ignoring balance")
-    df$Balance <- df$Epsilon
-  }
-  if (!("M" %in% colnames(df))) {
-    df$M <- -1
-  }
-  if (!("N" %in% colnames(df))) {
-    df$N <- -1
-  }
-  if (!("TimeIO" %in% colnames(df))) {
-    df$TimeIO <- -1
-  }
-  if (!("TimeTotal" %in% colnames(df))) {
-    df$TimeTotal <- -1
-  }
+        # Normalize columns
+        if (!("NumNodes" %in% colnames(df))) {
+                df$NumNodes <- 1
+        }
+        if (!("NumMPIsPerNode" %in% colnames(df))) {
+                df$NumMPIsPerNode <- 1
+        }
+        if (!("NumThreadsPerMPI" %in% colnames(df))) {
+                if ("Threads" %in% colnames(df)) {
+                        df$NumThreadsPerMPI <- df$Threads
+                } else if ("Cores" %in% colnames(df)) {
+                        df$NumThreadsPerMPI <- df$Cores
+                } else if ("NumThreads" %in% colnames(df)) {
+                        df$NumThreadsPerMPI <- df$NumThreads
+                } else {
+                        df$NumThreadsPerMPI <- 1
+                }
+        }
 
-  # Fix imbalance column for ParMETIS (which is 1+x instead if 0+x)
-  # if (name == "ParMETIS" & all(df$Balance >= 1.0 || df$Balance == 0)) {
-  # df$Balance <- df$Balance - 1
-  # }
+        if (!("Cores" %in% colnames(df))) {
+                df$Cores <- df$NumNodes * df$NumMPIsPerNode * df$NumThreadsPerMPI
+        }
 
-  df$NumPEs <- df$NumNodes *
-    df$NumMPIsPerNode *
-    df$NumThreadsPerMPI
-  df$Algorithm <- name
+        if (!("Timeout" %in% colnames(df))) {
+                df$Timeout <- FALSE
+        }
+        if (!("Failed" %in% colnames(df))) {
+                df$Failed <- FALSE
+        }
+        if (!("Seed" %in% colnames(df))) {
+                df$Seed <- 0
+        }
+        if (!("Epsilon" %in% colnames(df))) {
+                print("Warning: no Epsilon column; default to 3%")
+                df$Epsilon <- 0.03
+        }
+        if (ignore_balance) {
+                df$Epsilon <- 10000.0
+        }
 
-  df <- aggregate_data(df, 3600, weakscaling_aggregator)
+        # Old CSV files use "Balance" instead of "Imbalance"
+        if ("Balance" %in% colnames(df) & !("Imbalance" %in% colnames(df))) {
+                df$Imbalance <- df$Balance
+        } else if (!("Imbalance" %in% colnames(df))) {
+                print("Warning: ignoring balance")
+                df$Balance <- df$Epsilon
+        }
 
-  return(df %>% dplyr::arrange(Graph, K))
+        if (!("M" %in% colnames(df))) {
+                df$M <- -1
+        }
+        if (!("N" %in% colnames(df))) {
+                df$N <- -1
+        }
+
+        df$NumPEs <- df$NumNodes *
+                df$NumMPIsPerNode *
+                df$NumThreadsPerMPI
+
+        df$Algorithm <- name
+
+        df <- aggregate_data(df, 3600, weakscaling_aggregator)
+
+        return(df %>% dplyr::arrange(Graph, K))
 }
 
 DEFAULT_ASPECT_RATIO <- 2 / (1 + sqrt(5))
 
 create_theme_facet <- function(aspect_ratio = DEFAULT_ASPECT_RATIO) {
-  theme(
-    #aspect.ratio = aspect_ratio,
-    legend.background = element_blank(),
-    legend.title = element_blank(),
-    legend.box.spacing = unit(0.1, "cm"),
-    legend.text = element_text(size = 12, color = "black"),
-    plot.title = element_text(size = 16, hjust = 0, color = "black"),
-    plot.subtitle = element_text(size = 14, hjust = 0, color = "gray30"),
-    plot.caption = element_text(size = 12, hjust = 1, color = "gray60"),
-    panel.grid.major = element_line(linetype = "11", linewidth = 0.5, color = "gray"),
-    panel.grid.minor = element_blank(),
-    strip.background = element_blank(),
-    strip.text = element_text(size = 14),
-    axis.line = element_line(linewidth = 0.2, color = "black"),
-    axis.title.y = element_text(size = 12, vjust = 1.5, color = "black"),
-    axis.title.x = element_text(size = 12, vjust = 1.5, color = "black"),
-    axis.text.x = element_text(size = 12, angle = 0, hjust = 0.5, color = "black"),
-    axis.text.y = element_text(size = 12, color = "black")
-  )
+        theme(
+                # aspect.ratio = aspect_ratio,
+                legend.background = element_blank(),
+                legend.title = element_blank(),
+                legend.box.spacing = unit(0.1, "cm"),
+                legend.text = element_text(size = 12, color = "black"),
+                plot.title = element_text(size = 16, hjust = 0, color = "black"),
+                plot.subtitle = element_text(size = 14, hjust = 0, color = "gray30"),
+                plot.caption = element_text(size = 12, hjust = 1, color = "gray60"),
+                panel.grid.major = element_line(linetype = "11", linewidth = 0.5, color = "gray"),
+                panel.grid.minor = element_blank(),
+                strip.background = element_blank(),
+                strip.text = element_text(size = 14),
+                axis.line = element_line(linewidth = 0.2, color = "black"),
+                axis.title.y = element_text(size = 12, vjust = 1.5, color = "black"),
+                axis.title.x = element_text(size = 12, vjust = 1.5, color = "black"),
+                axis.text.x = element_text(size = 12, angle = 0, hjust = 0.5, color = "black"),
+                axis.text.y = element_text(size = 12, color = "black")
+        )
 }
 
 create_theme <- function(aspect_ratio = DEFAULT_ASPECT_RATIO) {
-  theme(
-    aspect.ratio = aspect_ratio,
-    legend.background = element_blank(),
-    legend.title = element_blank(),
-    # legend.margin = margin(-5, 0, 0, 0),
-    # legend.spacing.x = unit(0.01, "cm"),
-    # legend.spacing.y = unit(0.01, "cm"),
-    legend.box.spacing = unit(0.1, "cm"),
-    legend.text = element_text(size = 8, color = "black"),
-    plot.title = element_text(size = 16, hjust = 0, color = "black"),
-    plot.subtitle = element_text(size = 14, hjust = 0, color = "gray30"),
-    plot.caption = element_text(size = 12, hjust = 1, color = "gray50"),
-    strip.background = element_blank(),
-    strip.text = element_blank(),
-    panel.grid.major = element_line(linetype = "11", linewidth = 0.5, color = "grey"),
-    panel.grid.minor = element_blank(),
-    axis.line = element_line(linewidth = 0.2, color = "black"),
-    axis.title.y = element_text(size = 8, vjust = 1.5, color = "black"),
-    axis.title.x = element_text(size = 8, vjust = 1.5, color = "black"),
-    axis.text.x = element_text(angle = 0, hjust = 0.5, size = 8, color = "black"),
-    axis.text.y = element_text(size = 8, color = "black")
-  )
+        theme(
+                aspect.ratio = aspect_ratio,
+                legend.background = element_blank(),
+                legend.title = element_blank(),
+                # legend.margin = margin(-5, 0, 0, 0),
+                # legend.spacing.x = unit(0.01, "cm"),
+                # legend.spacing.y = unit(0.01, "cm"),
+                legend.box.spacing = unit(0.1, "cm"),
+                legend.text = element_text(size = 8, color = "black"),
+                plot.title = element_text(size = 16, hjust = 0, color = "black"),
+                plot.subtitle = element_text(size = 14, hjust = 0, color = "gray30"),
+                plot.caption = element_text(size = 12, hjust = 1, color = "gray50"),
+                strip.background = element_blank(),
+                strip.text = element_blank(),
+                panel.grid.major = element_line(linetype = "11", linewidth = 0.5, color = "grey"),
+                panel.grid.minor = element_blank(),
+                axis.line = element_line(linewidth = 0.2, color = "black"),
+                axis.title.y = element_text(size = 8, vjust = 1.5, color = "black"),
+                axis.title.x = element_text(size = 8, vjust = 1.5, color = "black"),
+                axis.text.x = element_text(angle = 0, hjust = 0.5, size = 8, color = "black"),
+                axis.text.y = element_text(size = 8, color = "black")
+        )
 }
 
 gm_mean <- function(x, na.rm = TRUE, zero.propagate = FALSE) {
+        gm_mean <- function(x, na.rm = TRUE, zero.propagate = FALSE) {
+                if (any(x < 0, na.rm = TRUE)) {
+                        return(NaN)
+                }
 
-  gm_mean <- function(x, na.rm = TRUE, zero.propagate = FALSE) {
-    if (any(x < 0, na.rm = TRUE)) {
-      return(NaN)
-    }
+                if (zero.propagate) {
+                        if (any(x == 0, na.rm = TRUE)) {
+                                return(0)
+                        }
+                        return(exp(mean(log(x[x != Inf]), na.rm = na.rm)))
+                } else {
+                        return(exp(sum(log(x[x > 0 & x != Inf]), na.rm = na.rm) / length(x)))
+                }
+        }
 
-    if (zero.propagate) {
-      if (any(x == 0, na.rm = TRUE)) {
-        return(0)
-      }
-      return(exp(mean(log(x[x != Inf]), na.rm = na.rm)))
-    } else {
-      return(exp(sum(log(x[x > 0 & x != Inf]), na.rm = na.rm) / length(x)))
-    }
-  }
+        hm_mean <- function(x) {
+                return(length(x) / sum(1.0 / x[x > 0]))
+        }
 
-  hm_mean <- function(x) {
-    return(length(x) / sum(1.0 / x[x > 0]))
-  }
+        if (any(x < 0, na.rm = TRUE)) {
+                return(NaN)
+        }
 
-  if (any(x < 0, na.rm = TRUE)) {
-    return(NaN)
-  }
-
-  if (zero.propagate) {
-    if (any(x == 0, na.rm = TRUE)) {
-      return(0)
-    }
-    return(exp(mean(log(x[x != Inf]), na.rm = na.rm)))
-  } else {
-    return(exp(sum(log(x[x > 0 & x != Inf]), na.rm = na.rm) / length(x)))
-  }
+        if (zero.propagate) {
+                if (any(x == 0, na.rm = TRUE)) {
+                        return(0)
+                }
+                return(exp(mean(log(x[x != Inf]), na.rm = na.rm)))
+        } else {
+                return(exp(sum(log(x[x > 0 & x != Inf]), na.rm = na.rm) / length(x)))
+        }
 }
 
 hm_mean <- function(x) {
-  return(length(x) / sum(1.0 / x[x > 0]))
+        return(length(x) / sum(1.0 / x[x > 0]))
 }
--- plots/throughput_plot.R
+++ plots/throughput_plot.R
@@ -1,99 +1,100 @@
 source("texify_graph_names.R")
 
-create_throughput_plot <- function(...,
-                                   primary_key = c("Graph", "K"),
-                                   column.graph = "Graph",
-                                   column.algorithm = "Algorithm",
-                                   column.time = "AvgTime",
-                                   column.failed = "Failed",
-                                   column.infeasible = "Infeasible",
-                                   column.timeout = "Timeout",
-                                   column.m = "M",
-                                   column.num_pes = "NumPEs",
-                                   colors = c(),
-                                   namer = texify_graph_names_smallk,
-                                   tex = FALSE,
-                                   tiny = FALSE) {
-  all_datasets <- list(...)
-  stopifnot(length(all_datasets) > 0)
+create_throughput_plot <- function(
+    ...,
+    primary_key = c("Graph", "K"),
+    column.graph = "Graph",
+    column.algorithm = "Algorithm",
+    column.time = "AvgTime",
+    column.failed = "Failed",
+    column.infeasible = "Infeasible",
+    column.timeout = "Timeout",
+    column.m = "M",
+    column.num_pes = "NumPEs",
+    colors = c(),
+    namer = texify_graph_names_smallk,
+    tex = FALSE,
+    tiny = FALSE) {
+        all_datasets <- list(...)
+        stopifnot(length(all_datasets) > 0)
 
-  # Sort by primary key
-  for (dataset in all_datasets) {
-    dataset <- dataset %>% dplyr::arrange_at(primary_key)
-  }
+        # Sort by primary key
+        for (dataset in all_datasets) {
+                dataset <- dataset %>% dplyr::arrange_at(primary_key)
+        }
 
-  # Check for consistent data
-  first_dataset <- all_datasets[[1]]
-  for (dataset in all_datasets) {
-    stopifnot(column.time %in% colnames(dataset))
-    stopifnot(column.m %in% colnames(dataset))
-    stopifnot(column.algorithm %in% colnames(dataset))
-    stopifnot(column.timeout %in% colnames(dataset))
-    stopifnot(column.infeasible %in% colnames(dataset))
-    stopifnot(column.failed %in% colnames(dataset))
-    stopifnot(column.graph %in% colnames(dataset))
-    stopifnot(column.num_pes %in% colnames(dataset))
+        # Check for consistent data
+        first_dataset <- all_datasets[[1]]
+        for (dataset in all_datasets) {
+                stopifnot(column.time %in% colnames(dataset))
+                stopifnot(column.m %in% colnames(dataset))
+                stopifnot(column.algorithm %in% colnames(dataset))
+                stopifnot(column.timeout %in% colnames(dataset))
+                stopifnot(column.infeasible %in% colnames(dataset))
+                stopifnot(column.failed %in% colnames(dataset))
+                stopifnot(column.graph %in% colnames(dataset))
+                stopifnot(column.num_pes %in% colnames(dataset))
 
-    stopifnot(!(NA %in% dataset[[column.time]]))
-    stopifnot(!(NaN %in% dataset[[column.time]]))
-    stopifnot(!(-Inf %in% dataset[[column.time]]))
-    stopifnot(!(0 %in% dataset[[column.time]]))
-    stopifnot(!any(dataset[[column.time]] <= 0))
+                stopifnot(!(NA %in% dataset[[column.time]]))
+                stopifnot(!(NaN %in% dataset[[column.time]]))
+                stopifnot(!(-Inf %in% dataset[[column.time]]))
+                stopifnot(!(0 %in% dataset[[column.time]]))
+                stopifnot(!any(dataset[[column.time]] <= 0))
 
-    stopifnot(nrow(dataset) == nrow(first_dataset))
-    stopifnot(dataset[, primary_key] == first_dataset[, primary_key])
-    stopifnot(dataset[[column.m]] == first_dataset[[column.m]])
-  }
+                stopifnot(nrow(dataset) == nrow(first_dataset))
+                stopifnot(dataset[, primary_key] == first_dataset[, primary_key])
+                stopifnot(dataset[[column.m]] == first_dataset[[column.m]])
+        }
 
-  data <- rbind(...) %>%
-    dplyr::filter(Timeout | !Failed) %>%
-    dplyr::select(
-      Algorithm = rlang::sym(column.algorithm),
-      Graph = rlang::sym(column.graph),
-      Time = rlang::sym(column.time),
-      M = rlang::sym(column.m),
-      Timeout = rlang::sym(column.timeout),
-      Infeasible = rlang::sym(column.infeasible),
-      NumPEs = rlang::sym(column.num_pes)
-    )
-  data <- data %>% dplyr::mutate(
-    Throughput = M / Time,
-    Feasibility = ifelse(!Infeasible & !Timeout,
-      "Feasible",
-      ifelse(Timeout,
-        "Timeout",
-        "Infeasible"
-      )
-    )
-  )
-  if (tex) {
-    data <- data %>% dplyr::mutate(Graph = namer(Graph))
-  }
+        data <- rbind(...) %>%
+                dplyr::filter(Timeout | !Failed) %>%
+                dplyr::select(
+                        Algorithm = rlang::sym(column.algorithm),
+                        Graph = rlang::sym(column.graph),
+                        Time = rlang::sym(column.time),
+                        M = rlang::sym(column.m),
+                        Timeout = rlang::sym(column.timeout),
+                        Infeasible = rlang::sym(column.infeasible),
+                        NumPEs = rlang::sym(column.num_pes)
+                )
+        data <- data %>% dplyr::mutate(
+                Throughput = M / Time,
+                Feasibility = ifelse(!Infeasible & !Timeout,
+                        "Feasible",
+                        ifelse(Timeout,
+                                "Timeout",
+                                "Infeasible"
+                        )
+                )
+        )
+        if (tex) {
+                data <- data %>% dplyr::mutate(Graph = namer(Graph))
+        }
 
-  # Create power-of-two y labels
-  min_max_throughput <- data %>% dplyr::summarize(Max = max(Throughput), Min = min(Throughput))
-  max_log2 <- ceiling(log2(min_max_throughput$Max))
-  min_log2 <- floor(log2(min_max_throughput$Min))
+        # Create power-of-two y labels
+        min_max_throughput <- data %>% dplyr::summarize(Max = max(Throughput), Min = min(Throughput))
+        max_log2 <- ceiling(log2(min_max_throughput$Max))
+        min_log2 <- floor(log2(min_max_throughput$Min))
 
-  y_breaks <- seq(min_log2, max_log2, by = 2)
-  y_labels <- c()
-  if (tex) {
-    y_labels <- paste0("$2^{", y_breaks, "}$")
-  } else {
-    y_labels <- paste0("2^", y_breaks)
-  }
+        y_breaks <- seq(min_log2, max_log2, by = 2)
+        y_labels <- c()
+        if (tex) {
+                y_labels <- paste0("$2^{", y_breaks, "}$")
+        } else {
+                y_labels <- paste0("2^", y_breaks)
+        }
 
-  p <- ggplot(data, aes(x = NumPEs, y = Throughput, color = Algorithm)) +
-    geom_line(aes(linetype = Graph)) +
-    geom_point(aes(shape = Feasibility)) +
-    xlab("Number of PEs") +
-    ylab("Throughput [Edges / s]") +
-    scale_y_continuous(trans = "log2", breaks = 2^y_breaks, labels = y_labels) +
-    scale_x_continuous(trans = "log2")
+        p <- ggplot(data, aes(x = NumPEs, y = Throughput, color = Algorithm)) +
+                geom_line(aes(linetype = Graph)) +
+                geom_point(aes(shape = Feasibility)) +
+                xlab("Number of PEs") +
+                ylab("Throughput [Edges / s]") +
+                scale_y_continuous(trans = "log2", breaks = 2^y_breaks, labels = y_labels) +
+                scale_x_continuous(trans = "log2")
 
-  if (length(colors) > 0) {
-    p <- p + scale_color_manual(name = "Algorithm", values = colors)
-  }
+        if (length(colors) > 0) {
+                p <- p + scale_color_manual(name = "Algorithm", values = colors)
+        }
 
-  return(p)
+        return(p)
 }
